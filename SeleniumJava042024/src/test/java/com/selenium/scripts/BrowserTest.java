6fa5acc23ae5ee2e2834bdb538acff17662556a9
nareshbabumandula <naresh223@gmail.com>
2024-04-23 07:55:22 +0530

    Intial Commit

---
+++ SeleniumJava042024/src/test/java/com/selenium/scripts/BrowserTest.java
@@ -0,0 +1,22 @@
+package com.selenium.scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver= new ChromeDriver();                 
+		driver.get("https://www.mycontactform.com");
+        driver.manage().window().maximize();
+		driver.findElement(By.id("user")).sendKeys("Phani");
+		Thread.sleep(1000);
+		driver.findElement(By.id("pass")).sendKeys("Secure*123");
+		Thread.sleep(1000);
+		driver.findElement(By.name("btnSubmit")).click();
+		Thread.sleep(1000);
+		driver.quit();	
+	}
+
+}
