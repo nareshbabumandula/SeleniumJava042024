37b198bdfd6c91dea91e29740f0e9322b23cd357
nareshbabumandula <naresh223@gmail.com>
2024-04-29 07:52:48 +0530

    29-Apr-2024: Naresh: Selenium WebDriver Hierrachy

--- SeleniumJava042024/src/test/java/com/selenium/scripts/BrowserTest.java
+++ SeleniumJava042024/src/test/java/com/selenium/scripts/BrowserTest.java
@@ -2,17 +2,23 @@ package com.selenium.scripts;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class BrowserTest {
 
 	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver= new ChromeDriver();                 
+		WebDriver driver= new ChromeDriver();  
+
 		driver.get("https://www.mycontactform.com");
         driver.manage().window().maximize();
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
-		driver.findElement(By.id("user")).sendKeys("Phani");
+        WebElement username = driver.findElement(By.id("user"));
+        System.out.println(username.isDisplayed());
+        System.out.println(username.isEnabled());
+        username.sendKeys("Phani");
 		Thread.sleep(1000);
 		driver.findElement(By.id("pass")).sendKeys("Secure*123");
 		Thread.sleep(1000);
